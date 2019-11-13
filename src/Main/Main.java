11ce5240efe5bcd7e2c8c022bb6502b7d185daa0
unknown <michael.boutboul@uha.fr>
2019-11-13 13:49:32 +0100

    test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("coucoucoucou");
         Application.launch(ViewHandler.class);
     }
 }
