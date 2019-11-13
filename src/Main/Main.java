cccc0fd9aa943227b36e13620e07052cd5b2e1f0
tet0 <theophile.braye@gmail.com>
2019-11-13 14:27:09 +0100

    Test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("L'app n'est pas encore lancé");
         Application.launch(ViewHandler.class);
         System.out.println("L'application se lance");
     }
